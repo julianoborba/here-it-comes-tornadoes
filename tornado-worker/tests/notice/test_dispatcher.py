937067dfee37bd9f043d910a5a0731a42f3e76cd
Juliano <juliano.sato@protonmail.com>
2022-02-28 01:13:47 -0300

    Implement Slack Webhook usage

--- tornado-worker/tests/notice/test_dispatcher.py
+++ tornado-worker/tests/notice/test_dispatcher.py
@@ -5,22 +5,25 @@ from worker.notice import dispatcher
 class DispatcherTest(TestCase):
 
     def test_should_dispatch_a_notice(self):
-        notices = [
-            {
-                'subject': 'Screamming guy system',
-                'message': 'Here it comes!',
-                'channel': 'FOOBAR123',
-                'receipt_handle': 'avptgxktxigerbawfbjlkiaenwzgignqdfj...'
-            },
-            {
-                'subject': 'Screamming guy system',
-                'message': 'Here it comes!',
-                'channel': 'FOOBAR123',
-                'receipt_handle': 'avptgxktxigerbawfbjlkiaenwzgignqdfj...'
-            }
-        ]
-        expected_result = True
+        notice = {
+            'subject': 'Screamming guy system',
+            'message': 'Here it comes!',
+            'channel': 'FOOBAR123',
+            'receipt_handle': 'avptgxktxigerbawfbjlkiaenwzgignqdfj...'
+        }
+        expected_slack_notice = {
+            'attachments': [
+                {
+                    'attachment_type': 'default',
+                    'color': '#ad1721',
+                    'text': 'Here it comes!'
+                }
+            ],
+            'channel': 'FOOBAR123',
+            'text': 'Screamming guy system',
+            'username': 'Guy'
+        }
 
-        result = dispatcher.dispatch(notices)
+        slack_notice = dispatcher.build_slack_notice(notice)
 
-        self.assertEqual(expected_result, result)
+        self.assertEqual(expected_slack_notice, slack_notice)
--- tornado-worker/worker/notice/dispatcher.py
+++ tornado-worker/worker/notice/dispatcher.py
@@ -1,4 +1,28 @@
+from os import getenv
+from requests import post
+
+SLACK_WEBHOOK_URL = getenv('SLACK_WEBHOOK_URL', 'https://hooks.slack.com/<your>/<slack>/<webhook>')
+
+
+def build_slack_notice(notice):
+    data = {
+        'text': notice['subject'],
+        'username': 'Guy',
+        'channel': notice['channel'],
+        'attachments': [
+            {
+                'text': notice['message'],
+                'attachment_type': 'default',
+                'color': '#ad1721',
+            }
+        ]
+    }
+    return data
+
+
 def dispatch(notices):
     for notice in notices:
-        print(notice)
-    return True
+        post(
+            SLACK_WEBHOOK_URL,
+            json=build_slack_notice(notice)
+        )
