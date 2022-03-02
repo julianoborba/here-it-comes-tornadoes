135e2ad928a06be51de2172ed46db2b3259e8c7b
Juliano <juliano.sato@protonmail.com>
2022-03-02 20:47:47 -0300

    Add Swagger API

--- tornado-api/go.mod
+++ tornado-api/go.mod
@@ -4,5 +4,13 @@ go 1.16
 
 require (
 	github.com/aws/aws-sdk-go v1.43.6 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
+	github.com/go-openapi/swag v0.21.1 // indirect
 	github.com/gorilla/mux v1.8.0
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/swaggo/http-swagger v1.2.5 // indirect
+	github.com/swaggo/swag v1.8.0 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
+	golang.org/x/tools v0.1.9 // indirect
 )
--- tornado-api/main.go
+++ tornado-api/main.go
@@ -10,97 +10,133 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger"
+
+	_ "tornado/docs"
 )
 
+var SQS_CLIENT = sqs.New(session.Must(session.NewSessionWithOptions(session.Options{
+	Config: aws.Config{
+		Endpoint: aws.String(os.Getenv(`QUEUE_URL`)),
+		Region:   aws.String(os.Getenv(`QUEUE_REGION`)),
+	},
+})))
+
+// Response defines the model for generic response message
 type Response struct {
-	Message string `json:"message"`
+	Message string `json:"message" example:"My generic user friendly message about response from some request"`
 }
 
+// Notice defines the model for a notice
 type Notice struct {
-	Origin  string `json:"origin"`
-	Message string `json:"message"`
-	Channel string `json:"channel"`
+	Subject string `json:"subject" example:"An EC2 instance is behaving in a manner indicating it is being used to perform a Denial of Service (DoS) attack using the TCP protocol."`
+	Finding string `json:"finding" example:"ThreatPurpose:ResourceTypeAffected/ThreatFamilyName.DetectionMechanism!Artifact"`
+	Channel string `json:"channel" example:"C05002EAE"`
 }
 
-func get_queue_url() string {
-	queue_url, _ := os.LookupEnv(`QUEUE_URL`)
-	return queue_url
+// EnqueuedMessage defines the model for a AWS SQS enqueued message
+type EnqueuedMessage struct {
+	MD5OfMessageAttributes       *string `json:"attributes_md5" example:"e64461b4cb51a781f7d35414369a7bfc"`
+	MD5OfMessageBody             *string `json:"body_md5" example:"f27eca4f499f59e0328f3f4ae35a4a1b"`
+	MD5OfMessageSystemAttributes *string `json:"system_attributes_md5" example:"null"`
+	MessageId                    *string `json:"id" example:"69069c03-8720-e75a-f386-3ca5b3d56801"`
+	SequenceNumber               *string `json:"sequence" example:"null"`
 }
 
-func get_queue_region() string {
-	queue_region, _ := os.LookupEnv(`QUEUE_REGION`)
-	return queue_region
+// Health godoc
+// @Summary Returns a indicator of health
+// @Description Returns HTTP 200 upon and only upon a successfully completed request
+// @Tags health
+// @Accept json
+// @Produce json
+// @Success 200 {object} Response
+// @Router /health [get]
+func health(w http.ResponseWriter, r *http.Request) {
+
+	w.WriteHeader(http.StatusOK)
+
+	var response Response
+	response.Message = "I am a healthy one!"
+
+	w.Header().Set("Content-Type", "application/json")
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	w.Write(jsonResponse)
+
 }
 
-func main() {
+// Notices godoc
+// @Summary Enqueues a new finding notice
+// @Description Parses JSON request body into a notice to be enqueued at AWS SQS for future consumption
+// @Tags notices
+// @Accept json
+// @Produce json
+// @Param notice body Notice true "Notice to enqueue"
+// @Success 200 {object} EnqueuedMessage
+// @Router /notices [post]
+func notices(w http.ResponseWriter, r *http.Request) {
 
-	queue_url := get_queue_url()
-	queue_region := get_queue_region()
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		Config: aws.Config{
-			Endpoint: aws.String(queue_url),
-			Region:   aws.String(queue_region),
+	dec := json.NewDecoder(r.Body)
+	var notice Notice
+	dec.Decode(&notice)
+
+	result, err := SQS_CLIENT.SendMessage(&sqs.SendMessageInput{
+		DelaySeconds: aws.Int64(10),
+		MessageAttributes: map[string]*sqs.MessageAttributeValue{
+			"Finding": {
+				DataType:    aws.String("String"),
+				StringValue: aws.String(notice.Finding),
+			},
+			"Channel": {
+				DataType:    aws.String("String"),
+				StringValue: aws.String(notice.Channel),
+			},
 		},
-	}))
-	svc := sqs.New(sess)
+		MessageBody: aws.String(notice.Subject),
+		QueueUrl:    aws.String(os.Getenv(`QUEUE_URL`)),
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var enqueued_message EnqueuedMessage
+	enqueued_message.MD5OfMessageAttributes = result.MD5OfMessageAttributes
+	enqueued_message.MD5OfMessageBody = result.MD5OfMessageBody
+	enqueued_message.MD5OfMessageSystemAttributes = result.MD5OfMessageSystemAttributes
+	enqueued_message.MessageId = result.MessageId
+	enqueued_message.SequenceNumber = result.SequenceNumber
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	jsonResponse, err := json.Marshal(enqueued_message)
+	if err != nil {
+		fmt.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	w.Write(jsonResponse)
+
+}
+
+// @title Tornado API
+// @version 1.0.0
+// @description API to enqueue findings with AWS SQS
+// @host localhost:8080
+// @BasePath /
+func main() {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/health-check", func(w http.ResponseWriter, r *http.Request) {
+	router.HandleFunc("/health", health).Methods("GET")
 
-		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/json")
-		var response Response
-		response.Message = "I am a healthy one."
-		jsonResponse, err := json.Marshal(response)
-		if err != nil {
-			return
-		}
-		w.Write(jsonResponse)
+	router.HandleFunc("/notices", notices).Methods("POST")
 
-	}).Methods("GET")
-
-	router.HandleFunc("/notice", func(w http.ResponseWriter, r *http.Request) {
-
-		dec := json.NewDecoder(r.Body)
-		var notice Notice
-		dec.Decode(&notice)
-
-		fmt.Printf("\nnotice received:\n%s\n", notice.Origin)
-
-		result, err := svc.SendMessage(&sqs.SendMessageInput{
-			DelaySeconds: aws.Int64(10),
-			MessageAttributes: map[string]*sqs.MessageAttributeValue{
-				"Message": {
-					DataType:    aws.String("String"),
-					StringValue: aws.String(notice.Message),
-				},
-				"Channel": {
-					DataType:    aws.String("String"),
-					StringValue: aws.String(notice.Channel),
-				},
-			},
-			MessageBody: aws.String(notice.Origin),
-			QueueUrl:    aws.String(queue_url),
-		})
-		if err != nil {
-			fmt.Printf("\nfailed to send message to queue:\n%s\n", err.Error())
-
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		fmt.Printf("\nmessage sent to queue\n")
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		jsonResponse, err := json.Marshal(result)
-		if err != nil {
-			return
-		}
-		w.Write(jsonResponse)
-
-	}).Methods("POST")
+	router.PathPrefix("/swagger").Handler(httpSwagger.WrapHandler)
 
 	http.Handle("/", router)
 
