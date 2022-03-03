023aeb8c9d71fb539c6ed49c58587c5db4c3023c
Juliano <juliano.sato@protonmail.com>
2022-03-02 21:42:50 -0300

    Adjust worker docker image

--- tornado-worker/setup.py
+++ tornado-worker/setup.py
@@ -4,12 +4,4 @@ setup(
     name='tornado-worker',
     version='v1.0.0',
     packages=['worker', 'worker.sqs', 'worker.notice'],
-    entry_points={
-        'console_scripts': ['worker=handler:handler']
-    },
-    url='',
-    license='',
-    author='',
-    author_email='',
-    description=''
 )
